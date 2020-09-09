c95730a8132a760bedeca4848210a6504c78d554
Mariano Cantero <marianojoaquinc@gmail.com>
2020-09-09 01:35:52 -0300

    primer commit

---
+++ template/app.js
@@ -0,0 +1,10 @@
+/* console.log('Hola mundo')
+
+const sidebar = document.querySelector('.sidebar');
+console.log(sidebar);
+
+document.addEventListener('click', openMenu);
+
+function openMenu(){
+    sidebar.classList.add('open');
+} */
